1182620898a838f477f7ddbf10a03383ebdb17c7
Henry Oberholtzer <henryoberholtzer@gmail.com>
2023-11-28 13:24:09 -0800

    added more features Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/index.js
+++ src/index.js
@@ -14,17 +14,30 @@ const getLocalStorage = (name) => {
 };
 
 window.onload = function() {
+    
+    const printPlants = (name) => {
+        const plantSelect = document.createElement("option");
+        plantSelect.innerText = name;
+        plantSelect.setAttribute("value", name);
+        const myPlants = document.getElementById("myPlants");
+        if (!localStorage.name) {
+            storeLocalState(name)(stateControl());
+        }
+        myPlants.append(plantSelect);
+    };
 
+    const printLocalStorage = () => {
+        for (let i = 0; i < localStorage.length; i++) {
+            const plantName = localStorage.key(i);
+            printPlants(plantName);
+        }
+    };
 
     document.getElementById('newPlant').onclick = function() {
         const plantName = document.getElementById('plant').value;
-        const plantSelect = document.createElement("option");
-        plantSelect.innerText = plantName;
-        plantSelect.setAttribute("value", plantName);
-        const myPlants = document.getElementById("myPlants");
-        storeLocalState(plantName)(stateControl());
-        myPlants.append(plantSelect);
+        printPlants(plantName);
     };
+
     document.getElementById('soil').onclick = function() {
         const accessedPlant = document.getElementById("myPlants").value;
         const newState = storeState(getLocalStorage(accessedPlant))(medSoil);
@@ -32,23 +45,36 @@ window.onload = function() {
         storeLocalState(accessedPlant)(newState);
     };
     document.getElementById('water').onclick = function() {
-        const newState = stateControl(medWater);
+        const accessedPlant = document.getElementById("myPlants").value;
+        const newState = storeState(getLocalStorage(accessedPlant))(medWater);
         document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+        storeLocalState(accessedPlant)(newState);
     };
     document.getElementById('feed').onclick = function() {
-        const newState = stateControl(medFood);
+        const accessedPlant = document.getElementById("myPlants").value;
+        const newState = storeState(getLocalStorage(accessedPlant))(medFood);
         document.getElementById('food-value').innerText = `Food: ${newState.food}`;
+        storeLocalState(accessedPlant)(newState);
     };
     document.getElementById('light').onclick = function() {
-        const newState = stateControl(medLight);
+        const accessedPlant = document.getElementById("myPlants").value;
+        const newState = storeState(getLocalStorage(accessedPlant))(medLight);
         document.getElementById('light-value').innerText = `Light: ${newState.light}`;
+        storeLocalState(accessedPlant)(newState);
     };
 
-    document.getElementById('show-state').onclick = function() {
-        const currentState = stateControl();
+    const showStats = (name) => {
+        const currentState = getLocalStorage(name);
         document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
         document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
         document.getElementById('food-value').innerText = `Food: ${currentState.food}`;
         document.getElementById('light-value').innerText = `Light: ${currentState.light}`;
     };
+
+    document.getElementById("myPlants").onchange = () => {
+        const accessedPlant = document.getElementById("myPlants").value;
+        showStats(accessedPlant);
+    };
+
+    printLocalStorage();
 };
